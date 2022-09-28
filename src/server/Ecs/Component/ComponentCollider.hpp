2176dac97f82091e8afc4525bf744e87f4f3f6ae
Laurent Enzo <enzo.laurent@epitech.eu>
2022-09-28 11:38:11 +0200

    [ADD] Implement collider component

---
+++ src/server/Ecs/Component/ComponentCollider.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** ComponentCollider.hpp
+** File description:
+** ComponentCollider.hpp
+*/
+
+#pragma once
+
+class ComponentCollider {
+    public:
+        ComponentCollider() = default;
+        ComponentCollider(const ComponentCollider &other) = default;
+        ~ComponentCollider() = default;
+        ComponentCollider &operator=(const ComponentCollider &other) = default;
+};
