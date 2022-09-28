5196eeaf3eb348d98eda7787a4ce186b801d990f
Laurent Enzo <enzo.laurent@epitech.eu>
2022-09-28 12:05:11 +0200

    [ADD] Implement shooter component

---
+++ src/server/Ecs/Component/Shooter.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** Shooter.hpp
+** File description:
+** Shooter.hpp
+*/
+
+#pragma once
+
+namespace component {
+    class Shooter {
+        public:
+            Shooter();
+            Shooter(const Shooter &other) = default;
+            ~Shooter() = default;
+            Shooter &operator=(const Shooter &other) = default;
+    };
+};
