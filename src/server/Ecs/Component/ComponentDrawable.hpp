b1b5bbb75de78579077e14771eb8a807f756a702
Laurent Enzo <enzo.laurent@epitech.eu>
2022-09-28 11:38:56 +0200

    [ADD] Implement drawable component

---
+++ src/server/Ecs/Component/ComponentDrawable.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** ComponentDrawable.hpp
+** File description:
+** ComponentDrawable.hpp
+*/
+
+#pragma once
+
+class ComponentDrawable {
+    public:
+        ComponentDrawable() = default;
+        ComponentDrawable(const ComponentDrawable &other) = default;
+        ~ComponentDrawable() = default;
+        ComponentDrawable &operator=(const ComponentDrawable &other) = default;
+};
