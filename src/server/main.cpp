ba1fd4a472efa0b1b7be863b8e33165435e6c88a
Marvin Verain <mverain@localhost.localdomain>
2022-09-28 15:39:52 +0200

    [TEST]: testing asio (network)

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,21 +5,66 @@
 ** main
 */
 
-#include "Ecs/ecs.hpp"
-#include "Network/network.hpp"
-#include "Ecs/Exceptions/Exception.hpp"
-#include <iostream>
 
-int main(void)
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+std::string make_daytime_string()
 {
-    try
-    {
-        my_ecs();
-        my_network();
-    } catch(const ecs::Exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << e.where() << std::endl;
-    }
-    return (0);
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
 }
+
+int main()
+{
+  try
+  {
+    boost::asio::io_context io_context;
+
+    udp::socket socket(io_context, udp::endpoint(udp::v4(), 13));
+
+    for (;;)
+    {
+      boost::array<char, 1> recv_buf;
+      udp::endpoint remote_endpoint;
+      socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
+
+      std::string message = make_daytime_string();
+
+      socket.send_to(boost::asio::buffer(message),
+          remote_endpoint, 0);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
+
+
+// #include "Ecs/ecs.hpp"
+// #include "Network/network.hpp"
+// #include "Ecs/Exceptions/Exception.hpp"
+// #include <iostream>
+
+// int main(void)
+// {
+//     try
+//     {
+//         my_ecs();
+//         my_network();
+//     } catch(const ecs::Exception &e)
+//     {
+//         std::cerr << e.what() << std::endl;
+//         std::cerr << e.where() << std::endl;
+//     }
+//     return (0);
+// }
