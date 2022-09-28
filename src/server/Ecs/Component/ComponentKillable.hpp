cf5fe65dc0ed5bea24c49418e96aaada61ee4960
Laurent Enzo <enzo.laurent@epitech.eu>
2022-09-28 11:39:19 +0200

    [ADD] Implement killable component

---
+++ src/server/Ecs/Component/ComponentKillable.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** ComponentKillable.hpp
+** File description:
+** ComponentKillable.hpp
+*/
+
+#pragma once
+
+class ComponentKillable {
+    public:
+        ComponentKillable() = default;
+        ComponentKillable(const ComponentKillable &other) = default;
+        ~ComponentKillable() = default;
+        ComponentKillable &operator=(const ComponentKillable &other) = default;
+};
