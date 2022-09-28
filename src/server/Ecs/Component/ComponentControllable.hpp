35419c246eea19bf89f6e10ed834c9f5a2b7258f
Laurent Enzo <enzo.laurent@epitech.eu>
2022-09-28 11:38:31 +0200

    [ADD] Implement controllable component

---
+++ src/server/Ecs/Component/ComponentControllable.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** ComponentControllable.hpp
+** File description:
+** ComponentControllable.hpp
+*/
+
+#pragma once
+
+class ComponentControllable {
+    public:
+        ComponentControllable() = default;
+        ComponentControllable(const ComponentControllable &other) = default;
+        ~ComponentControllable() = default;
+        ComponentControllable &operator=(const ComponentControllable &other) = default;
+};
