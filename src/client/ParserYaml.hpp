2ab301a02633314ec39afec5fc46c6ae9931bbf9
noaleclaire <noa.leclaire@epitech.eu>
2022-09-29 15:08:03 +0200

    [ADD] : ParserYaml class

---
+++ src/client/ParserYaml.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** ParserYaml
+** File description:
+** ParserYaml
+*/
+
+#pragma once
+
+class ParserYaml {
+  public:
+    ParserYaml();
+    ~ParserYaml();
+
+  protected:
+  private:
+};
