48759e52adda60e30d7d98124b38c088e9090439
noaleclaire <noa.leclaire@epitech.eu>
2022-10-06 16:46:55 +0200

    [FIX] : indentation in client/main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -32,14 +32,15 @@ int main(int ac, char **av)
     (void)ac;
     try {
         // assetsFolderExists();
+        graphics::SpritesManager sprites_manager;
+        ParserYaml::parseYaml(sprites_manager, std::filesystem::current_path() / "assets/sprites/sprites_config.yaml");
+        sprites_manager.printSpritesData();
+
         std::shared_ptr<void()> s;
         network_player net;
         std::thread f(&network_player::process_player, &net, s);
         window win;
         win.launch_window(s);
-        graphics::SpritesManager sprites_manager;
-        ParserYaml::parseYaml(sprites_manager, std::filesystem::current_path() / "assets/sprites/sprites_config.yaml");
-        sprites_manager.printSpritesData();
         f.join();
     } catch (const Exception &e) {
         std::cerr << e.what() << std::endl;
