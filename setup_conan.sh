7539c2228cddb4ccbadc9f4a91edf69bb85b832a
Marvin Verain <mverain@localhost.localdomain>
2022-10-17 15:20:38 +0200

    [UPD] changes on setup_conan_linux

--- setup_conan.sh
+++ setup_conan.sh
@@ -60,12 +60,14 @@ case $OS in
         sleep 0.15
     done
     echo -e "\033[0;"36"m \033[1;"96"m...Checking for dependencies..."
-    conan profile update settings.compiler.libcxx=libstdc++11 default
+    conan profile new default --detect
+    echo [conf]\ntools.system.package_manager:sudo=True\ntools.system.package_manager:mode=install >> ~/.conan/profiles/default
     for (( i = 30; i < 38; i++ ));
         do echo -ne "\033[0;"$i"m \033[1;"$i"m...Updating Profile..."; printf "\r"
         sleep 0.15
     done
     echo -e "\033[0;"31"m \033[1;"91"m...Updating Profile..."
+    sudo dnf install libfontenc-devel libXaw-devel libXcomposite-devel libXdmcp-devel libXtst-devel libxkbfile-devel libXres-devel libXScrnSaver-devel libXvMC-devel xorg-x11-xtrans-devel xcb-util-wm-devel xcb-util-keysyms-devel xcb-util-renderutil-devel libXdamage-devel xcb-util-devel xkeyboard-config-devel
     conan install .. --build missing
     for (( i = 30; i < 38; i++ ));
         do echo -ne "\033[0;"$i"m \033[1;"$i"m...Installing Useful Tools..."; printf "\r"
