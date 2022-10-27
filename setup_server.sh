0d84edc0dd30c2879a4abd7d871e10974387b670
Marvin Verain <mverain@localhost.localdomain>
2022-10-27 16:59:42 +0200

    [UPD]: updating firewall status in shell script

--- setup_server.sh
+++ setup_server.sh
@@ -2,8 +2,17 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    echo -ne "Server address : "
-    ip addr show | grep inet | head -n 3 | tail -n 1 | cut -d '/' -f 1 | cut -d ' ' -f 6
+    if [$0 -eq 0]
+    then
+        echo -ne "Server address : "
+        ip addr show | grep inet | head -n 3 | tail -n 1 | cut -d '/' -f 1 | cut -d ' ' -f 6
+        sudo systemctl stop firewalld.service
+        echo -e "Stop Firewall..."
+    fi
+    if [$0 -eq 1]
+    then
+        echo -e "Restart Firewall..."
+        sudo systemctl start firewalld.service
     ;;
     'WindowsNT')
     OS='Windows'
