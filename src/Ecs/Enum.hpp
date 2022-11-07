ca290e2c5c1b31c9c4751726005ff2da2fb2b926
Laurent Enzo <enzo.laurent@epitech.eu>
2022-11-07 10:14:21 +0100

    [EDIT] : add 2 new entity types and add the component to entities who doesn't have it

--- src/Ecs/Enum.hpp
+++ src/Ecs/Enum.hpp
@@ -10,5 +10,5 @@
 namespace ecs
 {
     enum Scenes { ROOM1, ROOM2, ROOM3, ROOM4, ROOM5, ROOM6, MENU, SETTINGS, WIN, LOOSE, GAME };
-    enum EntityTypes { SPACESHIP, MONSTER, SHOT, BUTTON, WALL, BACKGROUND };
+    enum EntityTypes { SPACESHIP, MONSTER, SHOT, BUTTON, WALL, BACKGROUND, SHAPE, TEXT };
 } // namespace ecs
