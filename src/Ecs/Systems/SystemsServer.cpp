2add529fb6ed305fb259f82f523e019e228fea2b
noaleclaire <noa.leclaire@epitech.eu>
2022-11-14 05:39:37 +0100

    [ADD] : check for os to setup_server

--- src/Ecs/Systems/SystemsServer.cpp
+++ src/Ecs/Systems/SystemsServer.cpp
@@ -9,26 +9,19 @@
 
 namespace ecs
 {
-    void SystemsServer::Kill(Registry &registry, SparseArray<ecs::Killable> &killable, CustomServer *server, std::vector<std::pair<udp::endpoint, bool>> clients_endpoint)
+    void SystemsServer::Kill(Registry &registry, SparseArray<ecs::Killable> &killable, CustomServer *server)
     {
+        server->_mtx.lock();
         for (auto &it : registry.getEntities()) {
             try {
                 killable.at(it);
                 if (killable.at(it).value().getLife() == 0) {
                     registry.killEntity(it);
-                    // server->_mtx.lock();
-                    for (auto &client_endpoint : clients_endpoint) {
-                        network::Message<network::CustomMessage> message;
-                        message.header.id = network::CustomMessage::KillAnEntity;
-                        message << it;
-                        server->send(message, client_endpoint.first);
-                        std::this_thread::sleep_for(std::chrono::milliseconds(ecs::Enum::ping_latency_ms));
-                    }
-                    // server->_mtx.unlock();
                 }
             } catch (const Exception &e) {
                 continue;
             }
         }
+        server->_mtx.unlock();
     }
 }
